f83fcb987e76a8ca1756d83d1340108d6efacf55
vinimaza <vinicius013.souza@gmail.com>
2022-06-20 00:27:25 -0300

    parte 1

--- 2ºBimestre/POO/Checkpoint_EcommerceV0/src/Programa.java
+++ 2ºBimestre/POO/Checkpoint_EcommerceV0/src/Programa.java
@@ -9,6 +9,7 @@ public class Programa {
         Cliente c1 = new Cliente("mar454656", "4hfghfgh5", "Vinicius", 20, "Rua das boas noites, 8898");
         c1.verificarLogin();
 
+
         System.out.println("Nome: " + c1.getNome() + " Idade: " + c1.getIdade() + " Endereço: " + c1.getEndereco());
 
         c1.alterarCadastro("vini", 11, "rua das tardes boas");
